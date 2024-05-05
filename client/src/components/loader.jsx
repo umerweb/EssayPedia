fca8eb955cff961383dc44e796c9e26c6e01c9db
umer.web <umer.web2@gmail.com>
2024-05-06 00:53:51 +0500

    nothing

--- client/src/components/loader.jsx
+++ client/src/components/loader.jsx
@@ -1,12 +1,22 @@
 import 'ldrs/ring'
 import 'ldrs/waveform'
+import { tailChase } from 'ldrs'
+tailChase.register()
+
 
 const loader = () => {
     return (
         <div className="min-h-[100vh] justify-center items-center flex">
             
-          <h1>Loading</h1>
+            
 
+
+<l-tail-chase
+  size="40"
+  speed="1.75" 
+  color="black" 
+></l-tail-chase>
+            
             <l-waveform
                 size="35"
                 stroke="3.5"
