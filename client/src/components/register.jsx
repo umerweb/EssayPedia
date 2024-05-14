fb3c10199d1db65e301bd4e2a249a8085fe626da
umer.web <umer.web2@gmail.com>
2024-05-15 02:55:41 +0500

    dd

--- client/src/components/register.jsx
+++ client/src/components/register.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react"
 import {  toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useNavigate } from "react-router-dom";
